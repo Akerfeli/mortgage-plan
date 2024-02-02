e113effb9beb8afb63ceffb18ff09b0184918bcf
Felicia Åkerfelt <felicia.akerfelt@ha.ax>
2024-02-02 18:48:57 +0200

    Initial commit for Spring Boot project

---
+++ mortgage-plan-backend/src/main/java/com/github/akerfeli/mortgageplanbackend/MortgagePlanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.akerfeli.mortgageplanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgagePlanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgagePlanBackendApplication.class, args);
+	}
+
+}
---
+++ mortgage-plan-backend/src/test/java/com/github/akerfeli/mortgageplanbackend/MortgagePlanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.akerfeli.mortgageplanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgagePlanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
